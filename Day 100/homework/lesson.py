cbeb9a35367a2915f5293f9d42a40b5832b2addf
KukavaNika <nkukava38@gmail.com>
2025-09-09 20:02:40 +0400

    first commit

---
+++ Day 100/homework/lesson.py
@@ -0,0 +1,26 @@
+#1) https://www.codewars.com/kata/514b92a657cdc65150000006/train/python
+
+#2) https://www.codewars.com/kata/523f5d21c841566fde000009/train/python
+
+#3) https://www.codewars.com/kata/5264d2b162488dc400000001/train/python
+ 
+#4) https://www.codewars.com/kata/541c8630095125aba6000c00/train/python
+
+#5) https://www.codewars.com/kata/5839edaa6754d6fec10000a2/train/python
+
+
+
+#1
+def positive_sum(arr):
+    """Return the sum of all positive numbers in arr."""
+    return sum(x for x in arr if x > 0)
+
+#2
+def make_negative(number):
+    """Return the negative of number (or zero stays zero)."""
+    return -abs(number)
+
+#3
+#ვერ ვქენი
+
+#4
